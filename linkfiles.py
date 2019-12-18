9d9c47fa91c38569f58e9d399823961ec532f1a4
Zhen Zhang <izgzhen@gmail.com>
2019-12-17 20:05:31 -0800

    Support skip

--- linkfiles.py
+++ linkfiles.py
@@ -6,10 +6,17 @@ from msbase.utils import load_json
 metadata = load_json(sys.argv[1])
 dirname = sys.argv[2]
 metadata_root_dir = sys.argv[3]
+drebin_outputs_dir = sys.argv[4]
 
 assert dirname in ["maldir", "gooddir"]
 
 os.makedirs(dirname, exist_ok=True)
 
+existing_drebin_outputs = [os.path.basename(f).replace(".data", ".apk") for f in glob.glob(drebin_outputs_dir + "/*.data") ]
+
 for sample in metadata:
-    os.symlink(metadata_root_dir + "/" + sample['apk'], dirname + "/" + os.path.basename(sample['apk']))
+    apk_name = os.path.basename(sample['apk'])
+    if apk_name in existing_drebin_outputs:
+        print("Skip existing " + apk_name)
+        continue
+    os.symlink(metadata_root_dir + "/" + sample['apk'], dirname + "/" + apk_name)
