f56b43843d231ab90ca736f4ba47548a3d1842af
Zhen Zhang <izgzhen@gmail.com>
2019-12-22 13:30:58 -0800

    More general linkfiles

--- linkfiles.py
+++ linkfiles.py
@@ -3,10 +3,13 @@ import sys
 import os
 from msbase.utils import load_json
 
-metadata = load_json(sys.argv[1])
+source_root_dir = sys.argv[1]
 dirname = sys.argv[2]
-metadata_root_dir = sys.argv[3]
-drebin_outputs_dir = sys.argv[4]
+drebin_outputs_dir = sys.argv[3]
+
+metadata = None
+if len(sys.argv) > 4:
+    metadata = load_json(sys.argv[4])
 
 assert dirname in ["maldir", "gooddir"]
 
@@ -14,9 +17,14 @@ os.makedirs(dirname, exist_ok=True)
 
 existing_drebin_outputs = [os.path.basename(f).replace(".data", ".apk") for f in glob.glob(drebin_outputs_dir + "/*.data") ]
 
-for sample in metadata:
-    apk_name = os.path.basename(sample['apk'])
+if metadata is not None:
+    apks = [ source_root_dir + "/" + sample["apk"] for sample in metadata ]
+else:
+    apks = glob.glob(source_root_dir + "/*.apk")
+
+for apk in apks:
+    apk_name = os.path.basename(apk)
     if apk_name in existing_drebin_outputs:
         print("Skip existing " + apk_name)
         continue
-    os.symlink(metadata_root_dir + "/" + sample['apk'], dirname + "/" + apk_name)
+    os.symlink(apk, dirname + "/" + apk_name)
