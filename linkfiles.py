66bb8a7ca8e7997ba3ac88ccabde6cce3b8cc240
Zhen Zhang <izgzhen@gmail.com>
2019-12-22 17:01:37 -0800

    Improve batch mode

--- linkfiles.py
+++ linkfiles.py
@@ -4,27 +4,31 @@ import os
 from msbase.utils import load_json
 
 source_root_dir = sys.argv[1]
-dirname = sys.argv[2]
-drebin_outputs_dir = sys.argv[3]
+drebin_outputs_dir = sys.argv[2]
 
 metadata = None
-if len(sys.argv) > 4:
-    metadata = load_json(sys.argv[4])
+if len(sys.argv) > 3:
+    if sys.argv[3].endswith("*.json"):
+        metadata = load_json(sys.argv[3])
+    elif os.path.isdir(sys.argv[3]):
+        metadata = [ s for m in glob.glob(sys.argv[3] + "/**/*.json", recursive=True) for s in load_json(m) ]
 
-assert dirname in ["maldir", "gooddir"]
-
-os.makedirs(dirname, exist_ok=True)
+for dirname in ["maldir", "gooddir"]:
+    os.makedirs(dirname, exist_ok=True)
 
 existing_drebin_outputs = [os.path.basename(f).replace(".data", ".apk") for f in glob.glob(drebin_outputs_dir + "/*.data") ]
 
 if metadata is not None:
-    apks = [ source_root_dir + "/" + sample["apk"] for sample in metadata ]
+    benign_apks = [ source_root_dir + "/" + sample["apk"] for sample in metadata if sample["label"] == "benign" ]
+    mal_apks = [ source_root_dir + "/" + sample["apk"] for sample in metadata if sample["label"] != "benign" ]
 else:
-    apks = glob.glob(source_root_dir + "/*.apk")
+    benign_apks = []
+    mal_apks = glob.glob(source_root_dir + "/*.apk")
 
-for apk in apks:
-    apk_name = os.path.basename(apk)
-    if apk_name in existing_drebin_outputs:
-        print("Skip existing " + apk_name)
-        continue
-    os.symlink(apk, dirname + "/" + apk_name)
+for dirname, apks in [("maldir", mal_apks), ("gooddir", benign_apks)]:
+    for apk in apks:
+        apk_name = os.path.basename(apk)
+        if apk_name in existing_drebin_outputs:
+            print("Skip existing " + apk_name)
+            continue
+        os.symlink(os.path.realpath(apk), dirname + "/" + apk_name)
